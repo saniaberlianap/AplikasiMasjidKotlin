65971bc8c2e341c95fdc9b997bda08116088d38c
saniaberlianap <saniabrlnn@gmail.com>
2020-05-13 11:03:16 +0700

    Bottom Sheet

---
+++ BottomSheets/app/src/main/java/com/example/bottomsheets/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.bottomsheets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+class MainActivity : AppCompatActivity() {
+
+    var mBottomSheetDialog: BottomSheetDialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun ShowBottomSheetDialog(view: View){
+        val bottomSheetLayout = layoutInflater.inflate(R.layout.bottom_sheet_dialog,null)
+        mBottomSheetDialog = BottomSheetDialog(this)
+        mBottomSheetDialog!!.setContentView(bottomSheetLayout)
+        mBottomSheetDialog!!.show()
+    }
+}
